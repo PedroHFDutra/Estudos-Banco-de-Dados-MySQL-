0bb6820c96f44fce08dc74a32e5ebfa444e0fe93
Pedro Henrique <106541103+PedroHFDutra@users.noreply.github.com>
2022-07-12 21:23:02 -0300

    Create Criando_Meu_1º_Banco.sql

---
+++ Criando_Meu_1º_Banco.sql
@@ -0,0 +1,10 @@
+/* Criando meu primeiro banco de dados através do MySQL Workbench*/
+
+create database <nome_do_database>;  //Comando para invocar a criação do database dentro do MySQL Workbench
+
+create table <nome_da_tabela> ( //Comando para invocar a criação da tabela do database, adicionando as colunas de dados a armazenar
+<info> <tipo_de_info>,
+<info> <tipo_de_info>
+<info> <tipo_de_info>
+<info> <tipo_de_info>
+);
